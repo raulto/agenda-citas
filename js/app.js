2e8282f1daae77ba51701f4026c7c6ef88e7540b
raulto <isc.torres.raul@gmail.com>
2024-05-09 21:06:28 -0700

    Cambio desde la pc del trabajo

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@ const emailInput = document.querySelector("#email");
 const fechaInput = document.querySelector("#fecha");
 const sintomasInput = document.querySelector("#sintomas");
 
+const formulario = document.querySelector("#formulario-cita");
+
 const citaObj = {
     paciente: '',
     propietario: '',
@@ -24,12 +26,24 @@ function eventListeners(){
     emailInput.addEventListener('change', datosCita);
     fechaInput.addEventListener('change', datosCita);   
     sintomasInput.addEventListener('change', datosCita);   
-
+    formulario.addEventListener("submit",cargarDatos);
 }
 
 function datosCita(e){
 
     citaObj[e.target.name] = e.target.value;
     console.log(citaObj);
+
+}
+
+function cargarDatos(e){
+
+    e.preventDefault();
+
+    if(Object.values(citaObj).some(valor => valor.trim() === '')){
+        console.log("Ningun valor debe ser vacio");
+        return;
+    }
+
 
 }
